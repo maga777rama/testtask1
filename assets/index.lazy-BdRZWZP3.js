0de674512e31fa2329f204fc3958880a26b24c8a
GlRamazanov Magomed <maga777rama@gmail.com>
2025-02-09 18:42:52 +0400

    Updates

--- assets/index.lazy-BdRZWZP3.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t}from"./index-DES43NJ_.js";const s=e("/cards/")({component:o});function o(){return t.jsxs("div",{children:["Страница с балбесами",t.jsx("h1",{})]})}export{s as Route};
