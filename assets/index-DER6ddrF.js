afdd725a8d3a445b19ccdaf89ca2457c9e84b98f
GlRamazanov Magomed <maga777rama@gmail.com>
2025-02-09 19:02:57 +0400

    Updates

---
+++ assets/index-DER6ddrF.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-D8xV5Edm.js";const r=function(){return n.jsxs("div",{children:["Страница с балбесами",n.jsx("h1",{})]})};export{r as component};
--- index3.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index.js";const r=function(){return n.jsxs("div",{children:["Страница с балбесами",n.jsx("h1",{})]})};export{r as component};
